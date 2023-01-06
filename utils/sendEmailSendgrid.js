38cd7a5e960c87ea187798cda5a7a6ac1f58fb4b
eshan1925 <eshan@getmedesign.com>
2023-01-07 03:57:53 +0530

    Minor fixes

--- utils/sendEmailSendgrid.js
+++ utils/sendEmailSendgrid.js
@@ -8,7 +8,7 @@ module.exports = async (receiver,subject, url) => {
       from: "eshan@getmedesign.com", // Change to your verified sender
       subject: subject,
       text: url,
-      html: `<strong>Please verify your account with the link-:<br /></strong><a>${url}</a>`,
+      html: `<strong>Please verify your account with the link-:<br /></strong><a href=${url}>${url}</a>`,
     };
     await sgMail.send(msg).then(() => {
       console.log("Email sent");
